81a2793a74e2aefb9ccf72403c675d7c18856d16
ÍõÑǴ³ <1975670198@qq.com>
2025-04-27 11:55:40 +0800

    axi rd wr fifo

---
+++ src/AXI读写/axi_read_fifo.v
@@ -0,0 +1,202 @@
+////////////////////////////////////////////////////////////////////////////////
+// File:	axi_read.v
+// Author:	FPGA_master <1975670198@qq.com>
+// Description:
+//	AXI Reading Model.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+module axi_read #
+(
+    parameter integer AR_FLIP_BYTE   = 0     ,//0：不翻转  1：翻转    //大小端是否翻转
+    parameter integer AR_ADDR_WIDTH	= 32    ,                      //地址位宽
+    parameter integer AR_DATA_WIDTH	= 64    ,//32,64,128           //数据位宽
+    parameter integer AR_LIN	    = 16     //1-256               //突发长度
+)
+(
+    //读脉冲信号
+    input                               i_wr_done       ,
+    //读出数据  
+    input                               M_RD_aclk       ,
+    input                               M_RD_aresetn    ,
+    output  wire                        M_RD_tlast      ,
+    output  wire                        M_RD_tvalid     ,
+    output  wire [AR_DATA_WIDTH-1:0]    M_RD_tdata      ,
+    input   wire                        M_RD_tready     , 
+    //AXI总线     
+    input   wire                        m_axi_aclk      ,
+    input   wire                        m_axi_aresetn   ,
+    output  wire                        m_axi_arid      ,
+    output  wire [AR_ADDR_WIDTH-1 : 0]  m_axi_araddr    ,
+    output  wire [7 : 0]                m_axi_arlen     ,
+    output  wire [2 : 0]                m_axi_arsize    ,
+    output  wire [1 : 0]                m_axi_arburst   ,
+    output  wire                        m_axi_arlock    ,
+    output  wire [3 : 0]                m_axi_arcache   ,
+    output  wire [2 : 0]                m_axi_arprot    ,
+    output  wire [3 : 0]                m_axi_arqos     ,
+    output  wire                        m_axi_arvalid   ,
+    input   wire                        m_axi_arready   ,
+    input   wire                        m_axi_rid       ,
+    input   wire [AR_DATA_WIDTH-1 : 0]  m_axi_rdata     ,
+    input   wire [1 : 0]                m_axi_rresp     ,
+    input   wire                        m_axi_rlast     ,
+    input   wire                        m_axi_rvalid    ,
+    output  wire                        m_axi_rready    
+);
+
+    //读地址和数据
+    reg     [31:0]              ar_addr     ;
+    reg     [7:0]               ar_len      ;
+    reg     [2:0]               ar_size     ;
+    reg     [1:0]               ar_burst    ;
+    reg                         ar_valid    ;
+    wire                        ar_ready    ;
+    wire    [AR_DATA_WIDTH-1:0] r_data      ;
+    wire                        r_resp      ;
+    wire                        r_last      ;
+    wire                        r_valid     ;
+    reg                         r_ready     ;
+
+    //读地址缓存和计数
+    reg     [31:0]              rd_addr_buff;
+    reg     [31:0]              num_rd_cnt  ;
+
+    //状态机
+    reg     [2:0]               c_state     ;
+    reg     [2:0]               n_state     ;
+    localparam  WAIT_RD     = 0,           
+                RD_ADDR     = 1,
+                RD_DATA     = 2,
+                RD_LAST     = 3,
+                RD_STOP     = 4;
+
+    //时钟、数据、突发信息传递
+    wire                        i_clk       ;
+    wire                        i_rst_n     ;
+    wire    [2:0]               arsize      ;
+    wire    [7:0]               arlen       ;
+    reg                         o_last      ;
+    reg                         o_valid     ;
+    wire                        i_ready     ;
+    reg     [AR_DATA_WIDTH-1:0]    o_data      ;
+
+    assign i_clk        = M_RD_aclk                 ;
+    assign i_rst_n      = M_RD_aresetn              ;
+    assign arsize       = clogb2((AR_DATA_WIDTH/8)-1);
+    assign arlen        = AR_LIN - 1                ;
+    assign M_RD_tlast   = o_last                    ;
+    assign M_RD_tvalid  = o_valid                   ;
+    assign i_ready      = M_RD_tready               ;
+	
+	//大小端转换
+	generate
+		if (AR_FLIP_BYTE == 1) begin
+			assign M_RD_tdata = {
+				o_data[7:0],     o_data[15:8], 
+				o_data[23:16],   o_data[31:24],
+				o_data[39:32],   o_data[47:40],
+				o_data[55:48],   o_data[63:56],
+				o_data[71:64],   o_data[79:72],
+				o_data[87:80],   o_data[95:88],
+				o_data[103:96],  o_data[111:104],
+				o_data[119:112], o_data[127:120]
+			};
+		end else begin
+			assign M_RD_tdata = o_data;  // 不翻转
+		end
+	endgenerate
+
+    //状态转换 FSM31
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if (~i_rst_n) c_state <= WAIT_RD;  // 异步复位
+        else          c_state <= n_state;   // 状态更新
+    end
+
+   //状态跳转条件 FSM32
+    always @(*) begin : R_FMS2        
+        case (c_state)
+            WAIT_RD:   n_state = (H2C_WR_NEXT != H2C_RD_NEXT) ? RD_ADDR : WAIT_RD;  // 检测到写完成则跳转
+            RD_ADDR:   n_state = ar_ready ? RD_DATA : RD_ADDR;                      // 地址准备好则跳转
+            RD_DATA:   n_state = (num_rd_cnt == ar_len-1 && o_valid && i_ready) ? RD_LAST : RD_DATA;  // 读完最后一笔数据则跳转
+            RD_LAST:   n_state = (o_valid && i_ready) ? RD_STOP : RD_LAST;          // 最后一笔传输完成则跳转
+            RD_STOP:   n_state = WAIT_RD;                                          // 自动回到等待状态
+            default:   n_state = 0;                                                 // 异常情况复位
+        endcase
+    end
+
+    //状态执行的操作 FSM33
+    always @(posedge i_clk or negedge i_rst_n) begin : R_FMS3
+        if (~i_rst_n) begin
+            {ar_addr, ar_len, ar_burst, ar_size, ar_valid, o_last} <= 0;
+            rd_addr_buff <= `RD_START_ADDR;
+        end
+        else case (n_state)
+            WAIT_RD: ar_valid <= 0;
+            
+            RD_ADDR: begin
+                ar_valid <= 1;
+                ar_addr  <= rd_addr_buff;
+                ar_len   <= arlen;
+                ar_burst <= 2'd1;
+                ar_size  <= arsize;
+            end
+    
+            RD_DATA: ar_valid <= 0;
+    
+            RD_LAST: o_last <= 1;
+    
+            RD_STOP: begin
+                o_last <= 0;
+                rd_addr_buff <= (rd_addr_buff >= `RD_END_ADDR - 4096) ? 0 : rd_addr_buff + 4096;
+            end
+        endcase
+    end
+ 
+    // data valid ready 处理    
+    always @(*) begin
+        r_ready = (c_state == RD_DATA || c_state == RD_LAST) ? i_ready : 0;
+        o_data  = (c_state == RD_DATA || c_state == RD_LAST) ? r_data  : 0;
+        o_valid = (c_state == RD_DATA || c_state == RD_LAST) ? r_valid : 0;
+    end
+
+    //读突发数据计数
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if (~i_rst_n)    num_rd_cnt <= 0;
+        else             num_rd_cnt <= o_last ? 0 : (r_valid && i_ready) ? num_rd_cnt + 1 : num_rd_cnt;
+    end
+
+    //----------------------------------------------------------
+    // 位宽计算函数
+    // 使用方法
+    // localparam AR_DATA_WIDTH = clogb2(depth); //数据位宽
+    //----------------------------------------------------------
+
+    function integer clogb2 (input integer depth);
+    begin
+        for(clogb2=0; depth>0; clogb2=clogb2+1) 
+            depth = depth >> 1;
+    end
+    endfunction
+
+    assign m_axi_araddr     = ar_addr       ;
+    assign m_axi_arlen      = ar_len        ;
+    assign m_axi_arsize     = ar_size       ;
+    assign m_axi_arburst    = ar_burst      ;
+    assign m_axi_arvalid    = ar_valid      ;
+    assign ar_ready         = m_axi_arready ;
+    
+    assign r_data           = m_axi_rdata   ;
+    assign r_last           = m_axi_rlast   ;
+    assign r_resp           = m_axi_rresp   ;
+    assign r_valid          = m_axi_rvalid  ;
+    assign m_axi_rready     = r_ready       ;
+  
+
+	assign m_axi_arid       = 0;
+	assign m_axi_arlock     = 0;
+	assign m_axi_arcache    = 3;
+	assign m_axi_arprot     = 0;
+	assign m_axi_arqos      = 0;
+
+endmodule
---
+++ src/AXI读写/axi_write_fifo.v
@@ -0,0 +1,225 @@
+////////////////////////////////////////////////////////////////////////////////
+// File:	axi_write.v
+// Author:	FPGA_master <1975670198@qq.com>
+// Description:
+//	AXI Writing Model.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+module axi_write #
+(
+    parameter integer FLIP_BYTE   = 0     ,//0：不翻转  1：翻转    //大小端是否翻转
+    parameter integer ADDR_WIDTH	= 32    ,                      //地址位宽
+    parameter integer DATA_WIDTH	= 64    ,//32,64,128           //数据位宽
+    parameter integer AW_LIN	    = 16     //1-256               //突发长度
+)
+(        
+    //写入数据
+    input                               S_WR_aclk       ,
+    input                               S_WR_aresetn    ,
+    input   wire [DATA_WIDTH-1:0]       S_WR_tdata      ,
+    input                               S_WR_tvalid     ,
+    input                               S_WR_tlast      ,
+    output                              S_WR_tready     ,
+    //AXI总线 
+    input   wire                        m_axi_aclk      ,
+    input   wire                        m_axi_aresetn   ,
+    output  wire                        m_axi_awid      ,
+    output  wire [ADDR_WIDTH-1 : 0]     m_axi_awaddr    ,
+    output  wire [7 : 0]                m_axi_awlen     ,
+    output  wire [2 : 0]                m_axi_awsize    ,
+    output  wire [1 : 0]                m_axi_awburst   ,
+    output  wire                        m_axi_awlock    ,
+    output  wire [3 : 0]                m_axi_awcache   ,
+    output  wire [2 : 0]                m_axi_awprot    ,
+    output  wire [3 : 0]                m_axi_awqos     ,
+    output  wire                        m_axi_awvalid   ,
+    input   wire                        m_axi_awready   ,
+    output  wire [DATA_WIDTH-1 : 0]     m_axi_wdata     ,
+    output  wire [DATA_WIDTH/8-1 : 0]   m_axi_wstrb     ,
+    output  wire                        m_axi_wlast     ,
+    output  wire                        m_axi_wvalid    ,
+    input   wire                        m_axi_wready    ,
+    input   wire                        m_axi_bid       ,
+    input   wire [1 : 0]                m_axi_bresp     ,
+    input   wire                        m_axi_bvalid    ,
+    output  wire                        m_axi_bready    
+);
+
+    //写数据    
+    reg     [DATA_WIDTH-1:0]    w_data      ;
+    reg                         w_valid     ;
+    wire                        w_ready     ;
+    reg                         w_last      ;
+    reg     [DATA_WIDTH/8-1:0]  w_strb      ; // 写字节选通信号 64位：1111_1111 (一个字节一位，64bit位8字节，所以是八位)
+
+    //写地址    
+    reg     [ADDR_WIDTH-1:0]    aw_addr     ;
+    reg     [7:0]               aw_len      ; // 突发长度类型：传输8个数据，则len=7
+    reg     [2:0]               aw_size     ; // 传输中的字节数 011 ：8B
+    reg     [1:0]               aw_burst    ; // 突发类型      01 ：递增突发
+    reg                         aw_valid    ;
+    wire                        aw_ready    ;
+    
+    //写响应        
+    wire    [1:0]               b_resp      ;
+    wire                        b_valid     ;
+    reg                         b_ready     ;
+
+    //计数器
+    reg     [11:0]  number_cnt  ;       // 一次突发传输的数据计数
+    reg     [31:0]  aw_addr_cnt ;       // 写地址计数
+
+    //状态机
+    reg     [2:0]   c_state     ;
+    reg     [2:0]   n_state     ;
+    localparam  WR_IDLE         =   3'd0,
+                WR_ADDR         =   3'd2,
+                WR_DATA         =   3'd3,
+                WR_LAST         =   3'd4,
+                WR_STOP         =   3'd5;
+
+    //时钟、数据、突发信息传递
+    wire    [DATA_WIDTH-1:0]    i_data      ;
+    wire                        i_valid     ;
+    reg                         o_ready     ;
+    wire                        i_last      ;
+    wire    [2:0]               awsize      ;
+    wire    [7:0]               awlen       ;
+    wire    [DATA_WIDTH/8-1:0]  wstrb       ;
+
+    assign wstrb        = {(DATA_WIDTH/8){1'b1}}    ;
+    assign awsize       = clogb2((DATA_WIDTH/8)-1)  ;
+    assign awlen        = AW_LIN - 1                ;
+    assign i_clk        = S_WR_aclk                 ;
+    assign i_rst_n      = S_WR_aresetn              ;
+    assign i_valid      = S_WR_tvalid               ;
+    assign i_last       = S_WR_tlast                ;
+    assign S_WR_tready  = o_ready                   ;
+	
+	//大小端转换
+	generate
+		if (FLIP_BYTE == 1) begin
+			assign i_data = {
+				S_WR_tdata[7:0],     S_WR_tdata[15:8], 
+				S_WR_tdata[23:16],   S_WR_tdata[31:24],
+				S_WR_tdata[39:32],   S_WR_tdata[47:40],
+				S_WR_tdata[55:48],   S_WR_tdata[63:56],
+				S_WR_tdata[71:64],   S_WR_tdata[79:72],
+				S_WR_tdata[87:80],   S_WR_tdata[95:88],
+				S_WR_tdata[103:96],  S_WR_tdata[111:104],
+				S_WR_tdata[119:112], S_WR_tdata[127:120]
+			};
+		end else begin
+			assign i_data = S_WR_tdata;  // 不翻转
+		end
+	endgenerate
+
+    //状态转换 FSM31
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if (~i_rst_n) c_state <= WR_IDLE;  // 异步复位
+        else          c_state <= n_state;   // 状态更新
+    end
+   
+    // 写操作状态机组合逻辑 FSM32
+    always @(*) begin : W_FMS2
+        case (c_state)
+            WR_IDLE:  n_state = i_valid ? WR_ADDR : WR_IDLE;  // 有数据则进入地址发送状态            
+            WR_ADDR:  n_state = aw_ready ? WR_DATA : WR_ADDR; // 地址准备好则进入数据传输            
+            WR_DATA:  n_state = (num_wr_cnt == aw_len-1 && w_ready && w_valid) ? WR_LAST : WR_DATA;  // 最后一笔数据时进入结束状态            
+            WR_DELAY: n_state = i_valid ? WR_DATA : WR_DELAY; // 有新数据则继续传输            
+            WR_LAST:  n_state = (w_ready && w_valid && w_last) ? WR_STOP : WR_LAST;  // 完成最后传输后进入停止状态            
+            WR_STOP:  n_state = WR_IDLE;  // 传输完全结束后回到初始状态           
+            default:  n_state = 'bx;  // 异常情况处理
+        endcase
+    end
+
+    // 写操作状态机时序逻辑
+    // 功能：管理AXI写通道控制信号
+    always @(posedge i_clk or negedge i_rst_n) begin : W_FMS3
+        // 异步复位逻辑
+        if (~i_rst_n) begin              
+            // 复位所有AXI写通道信号
+            {w_strb, aw_addr, aw_len, aw_size, aw_burst, aw_valid, w_last} <= 0;
+            // 初始化写地址计数器
+            aw_addr_cnt <= `WR_START_ADDR; 
+        end
+        else case (n_state)            
+            WR_ADDR : begin  // 地址发送状态
+                w_strb   <= wstrb;      // 设置写数据选通信号
+                aw_size  <= awsize;     // 设置传输数据大小
+                aw_burst <= 2'd1;       // 固定为增量突发模式
+                aw_len   <= awlen;      // 设置突发长度
+                aw_valid <= 1;          // 置起地址有效信号
+                aw_addr  <= aw_addr_cnt; // 输出当前地址
+            end
+            
+            WR_DATA :   aw_valid <= 0;          // 地址通道握手完成           
+            WR_LAST :   w_last <= 1;            // 标记最后一笔数据              
+            WR_STOP : begin // 传输结束状态
+                w_last  <= 0;           // 清除最后数据标记
+                // 地址计数器循环处理
+                aw_addr_cnt <= (aw_addr_cnt >= `WR_END_ADDR-4096) ? 
+                              0 : aw_addr_cnt + 4096;
+            end    
+            default: ; // 保持原有状态
+        endcase
+    end
+    
+    //突发数据计数  
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if (~i_rst_n)    num_wr_cnt <= 0;
+        else             num_wr_cnt <= w_last ? 0 : (w_valid && w_ready) ? num_wr_cnt + 1 : num_wr_cnt;
+    end
+
+    //data valid ready信号处理
+    always @(*) begin
+        o_ready = (c_state == WR_DATA || c_state == WR_LAST) ? w_ready : 0;
+        w_data  = (c_state == WR_DATA || c_state == WR_LAST) ? i_data  : 0;
+        w_valid = (c_state == WR_DATA || c_state == WR_LAST) ? i_valid : 0;
+    end
+
+    //写响应处理
+    always @ (posedge i_clk, negedge i_rst_n) begin
+        if (~i_rst_n)   b_ready <= 0;
+        else            b_ready <= 1;
+    end
+
+    //----------------------------------------------------------
+    // 位宽计算函数
+    // 使用方法
+    // localparam DATA_WIDTH = clogb2(depth); //数据位宽
+    //----------------------------------------------------------
+
+    function integer clogb2 (input integer depth);
+    begin
+        for(clogb2=0; depth>0; clogb2=clogb2+1) 
+            depth = depth >> 1;
+    end
+    endfunction
+    
+    assign m_axi_wdata      = w_data        ;
+	assign m_axi_wvalid     = w_valid       ;
+	assign m_axi_wlast      = w_last        ;
+	assign m_axi_wstrb      = w_strb        ;
+	assign w_ready          = m_axi_wready  ;
+
+	assign m_axi_awaddr     = aw_addr       ;
+	assign m_axi_awlen      = aw_len        ;
+	assign m_axi_awsize     = aw_size       ;
+	assign m_axi_awburst    = aw_burst      ;
+	assign m_axi_awvalid    = aw_valid      ;
+	assign aw_ready         = m_axi_awready ;
+
+	assign b_resp           = m_axi_bresp   ;
+	assign b_valid          = m_axi_bvalid  ;
+	assign m_axi_bready     = b_ready       ;
+
+
+    assign m_axi_awid       = 0;
+    assign m_axi_awlock     = 0;
+    assign m_axi_awcache    = 3;
+    assign m_axi_awprot     = 0;
+    assign m_axi_awqos      = 0;
+
+endmodule
